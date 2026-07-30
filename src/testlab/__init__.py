ec3151c742756fa122588f93103265a3ea2a3d8a
Jared Mahotiere <jmahotie@purdue.edu>
2026-07-30 16:36:20 -0400

    Add testlab package

---
+++ src/testlab/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable client and test-harness utilities for the synthetic DUT."""
+
+from testlab.client import LabClient, LabCommunicationError, LabResponse
+
+__all__ = ["LabClient", "LabCommunicationError", "LabResponse"]
